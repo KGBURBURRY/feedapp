5077269530874bdc0639160c3969c7b0e1e04484
karimgalal <galal.karim@outlook.com>
2022-12-02 16:33:32 -0500

    11.14

--- src/main/java/com/bptn/repository/UserRepository.java
+++ src/main/java/com/bptn/repository/UserRepository.java
@@ -1,12 +1,17 @@
 package com.bptn.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bptn.models.UserID;
 
+
+
 @Repository
 public interface UserRepository extends JpaRepository<UserID, String> {
 
+	@Query(value = "select * from public.\"UserID\" where username=?1", nativeQuery = true)
+	UserID findByUsername(String userName);
 	
 }
--- src/main/java/com/bptn/service/UserService.java
+++ src/main/java/com/bptn/service/UserService.java
@@ -1,43 +1,29 @@
 package com.bptn.service;
 
-import java.util.Optional;
-
+import com.bptn.exceptions.InvalidUserNameException;
+import com.bptn.models.UserID;
+import com.bptn.repository.UserRepository;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.bptn.exceptions.InvalidUserNameException;
-import com.bptn.models.UserID;
-import com.bptn.repository.UserRepository;
-
-import ch.qos.logback.classic.Logger;
 
 @Service
-
 public class UserService {
 
-	@Autowired
-	UserRepository userRepository;
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-	private Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private UserRepository userRepository;
 
-	public void validateUserId(String username) throws InvalidUserNameException {
-
-		//String message;
-
-		Optional<UserID> opt = this.userRepository.findById(username);
-
-		if (opt.isEmpty()) {
-			logger.error("User does not exist", username);
-			throw new InvalidUserNameException("User does not exist. Please try again");
-
-		} else {
-			logger.error("User Validated", username);
-
-		}
-
-
-
-	}
-
+    public void validateUserId(String username) throws InvalidUserNameException {
+        UserID userID = userRepository.findByUsername(username);
+        if (userID == null) {
+            LOGGER.error("Username={} doesn't exist", username);
+            throw new InvalidUserNameException("Username doesn't exist");
+        } else {
+            LOGGER.debug("Username={} validated", username);
+        }
+    }
 }
